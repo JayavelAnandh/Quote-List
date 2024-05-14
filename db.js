43b2f64fe974b64c09fb36147101b9f0cce06552
Jayavel Anandh <jayavel.anandh@changecx.com>
2024-05-14 15:37:59 +0530

    Updated the code to check exisiting user

--- db.js
+++ db.js
@@ -3,12 +3,8 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 function connectDataBase() {
-  const params = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  };
   try {
-    mongoose.connect(process.env.MONGO_DB_URL, params);
+    mongoose.connect(process.env.MONGO_DB_URL);
     console.log("DB is connected !");
   } catch (error) {
     console.log(error);
